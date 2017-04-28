039ed27b078e1fefa1a2eccb58e1b99ecc9c11ca
Mohamed Mellouki <mellouki.mhd@gmail.com>
2017-04-28 14:59:26 -0400

    update per #952 recommandation

--- js/adapt-contrib-responsiveIframe.js
+++ js/adapt-contrib-responsiveIframe.js
@@ -1,46 +1,57 @@
-/*
-* adapt-contrib-responsiveIframe
-* License - http://github.com/adaptlearning/adapt_framework/LICENSE
-* Maintainers - Kevin Corry <kevinc@learningpool.com>
-*/
+/**
+ * adapt-contrib-responsiveIframe
+ * License - http://github.com/adaptlearning/adapt_framework/LICENSE
+ * Maintainers - Kevin Corry <kevinc@learningpool.com>
+ */
 define(function(require) {
 
-    var ComponentView = require("coreViews/componentView");
-    var Adapt = require("coreJS/adapt");
+	var ComponentView = require("coreViews/componentView");
+	var Adapt = require("coreJS/adapt");
 
-    var ResponsiveIframe = ComponentView.extend({
+	var ResponsiveIframe = ComponentView.extend({
 
-        events: {
-            'inview':'inview'
-        },
+		events: {
+			//'inview':'inview'
+		},
 
-        preRender: function() {
-            this.listenTo(Adapt, 'device:changed', this.resizeControl);
-        },
+		preRender: function() {
+			this.listenTo(Adapt, 'device:changed', this.resizeControl);
+		},
 
-        postRender: function() {
-            var that = this;
-            this.$('.responsiveIframe-iframe').ready(function() {
-                that.resizeControl(Adapt.device.screenSize);
-                that.setReadyStatus();
-            });
-        },
+		postRender: function() {
+			var that = this;
+			this.$('.responsiveIframe-iframe').ready(function() {
+				that.resizeControl(Adapt.device.screenSize);
+				that.setReadyStatus();
+			});
+			//listen for the ifarme actions complation.
+			this.$('.responsiveIframe-iframe').on('completion:status',_.bind(this.iframeEventCompletion,this));
+		},
 
-        inview: function(event, visible) {
-            if (visible) {
-                this.setCompletionStatus();
-            }
-        },
+		inview: function(event, visible) {
+			/**
+			 * if (visible) {
+			 *	this.setCompletionStatus();
+			 * }
+			 */
+		},
 
-        resizeControl: function(size) {
-            var width = this.$('.responsiveIframe-iframe').attr('data-width-' + size);
-            var height = this.$('.responsiveIframe-iframe').attr('data-height-' + size);
-            this.$('.responsiveIframe-iframe').width(width);
-            this.$('.responsiveIframe-iframe').height(height);
-        }
+		iframeEventCompletion:function(event,complationStatus){
+			if(complationStatus){
+				this.setCompletionStatus();
+			}
 
-    });
+		},
 
-    Adapt.register("responsiveIframe", ResponsiveIframe);
+		resizeControl: function(size) {
+			var width = this.$('.responsiveIframe-iframe').attr('data-width-' + size);
+			var height = this.$('.responsiveIframe-iframe').attr('data-height-' + size);
+			this.$('.responsiveIframe-iframe').width(width);
+			this.$('.responsiveIframe-iframe').height(height);
+		}
+
+	});
+
+	Adapt.register("responsiveIframe", ResponsiveIframe);
 
 });
